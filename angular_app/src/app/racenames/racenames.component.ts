04979fbf8f2352c54d9b49583eb1ada63036b040
Kengo <jianwusongdao@gmail.com>
2023-07-14 05:02:20 +0900

    bag

--- angular_app/src/app/racenames/racenames.component.ts
+++ angular_app/src/app/racenames/racenames.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RaceService } from '../race.service';
 
+
+interface RaceNameObj{
+  date: string
+  dayOfTheWeek: string
+  name: string
+  num: string
+}
+
 @Component({
   selector: 'app-racenames',
   templateUrl: './racenames.component.html',
@@ -9,14 +17,28 @@ import { RaceService } from '../race.service';
 export class RacenamesComponent implements OnInit {
   constructor( private raceService: RaceService ){}
 
-  raceNames: any[] = [];
+  raceNames: RaceNameObj[] = [];
 
   ngOnInit(): void{
     this.getRaceNames();
   }
 
+
+
   getRaceNames(): void{
-    this.raceService.getRaceNames().subscribe((data)=> {this.raceNames=data})
+    this.raceService.getRaceNames().subscribe((data)=> {
+      for(let race of data){
+        let raceName = race.ramce_name;
+        let raceNameArray = raceName.split(/[\(\)\ ]/).filter(Boolean);
+        let raceNameObj: RaceNameObj = {
+          date: raceNameArray[0],
+          dayOfTheWeek: raceNameArray[1],
+          name: raceNameArray[2],
+          num: raceNameArray[3]
+        };
+        this.raceNames.push(raceNameObj);
+      }
+    })
   }
 
 }
