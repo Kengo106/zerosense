0d6c0dc081168f0a45db612f9c051e816b265557
Kengo <jianwusongdao@gmail.com>
2023-07-27 00:56:07 +0900

    0727-1

--- angular_app/src/app/service/session.service.ts
+++ angular_app/src/app/service/session.service.ts
@@ -6,71 +6,72 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { auth } from 'firebaseui';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class SessionService {
+    public session = new Session();
+    public sessionSubject = new Subject<Session>();
+    public sessionState = this.sessionSubject.asObservable();
 
-  public session = new Session();
-  public sessionSubject = new Subject<Session>()
-  public sessionState = this.sessionSubject.asObservable();
+    constructor(private router: Router, private afAuth: AngularFireAuth) {}
 
-  constructor(
-    private router: Router,
-    private afAuth: AngularFireAuth
-    ) { }
-
-  login(account : Password): void {
-    this.afAuth.signInWithEmailAndPassword(account.email, account.password).then(
-      auth => {
-            if(!auth.user?.emailVerified){
-              this.afAuth.signOut();
-              console.log(account.email)
-              return Promise.reject('メールアドレスが登録できません')
-            }else{
-              this.session.login = true;
-              this.sessionSubject.next(this.session);
-              return this.router.navigate(['/'])
-            }}
-            ).then(()=>alert('ログインしました'))
-            .catch( err => {
-              console.log(err);
-              alert('ログインに失敗しました。\n' + err);
+    login(account: Password): void {
+        this.afAuth
+            .signInWithEmailAndPassword(account.email, account.password)
+            .then((auth) => {
+                if (!auth.user?.emailVerified) {
+                    this.afAuth.signOut();
+                    console.log(account.email);
+                    return Promise.reject('メールアドレスが登録できません');
+                } else {
+                    this.session.login = true;
+                    this.sessionSubject.next(this.session);
+                    return this.router.navigate(['/']);
+                }
             })
-  }
+            .then(() => alert('ログインしました'))
+            .catch((err) => {
+                console.log(err);
+                alert('ログインに失敗しました。\n' + err);
+            });
+    }
 
-  logout(): void{
-    this.afAuth.signOut()
-    .then(() => {
-      this.sessionSubject.next(this.session.reset());
-      return this.router.navigate(['/account/login']);
-    }).then(()=>alert("ログアウトしました"))
-    .catch(err => {
-      console.log(err);
-      alert('ログアウトに失敗しました /n' + err);
+    logout(): void {
+        this.afAuth
+            .signOut()
+            .then(() => {
+                this.sessionSubject.next(this.session.reset());
+                return this.router.navigate(['/account/login']);
+            })
+            .then(() => alert('ログアウトしました'))
+            .catch((err) => {
+                console.log(err);
+                alert('ログアウトに失敗しました /n' + err);
+            });
+    }
 
-    });
-  }
-
-  signup(account: Password): Promise<void> {
-    return this.afAuth.createUserWithEmailAndPassword(account.email, account.password)
-    .then( (userCredential) => {
-      if(userCredential.user) {
-        console.log(account.username + "です")
-        return Promise.all([
-          userCredential.user.sendEmailVerification(),
-          userCredential.user.updateProfile({
-            displayName: account.username
-          })
-        ])
-      }else{throw new Error("Username is not provided.") }
-    } )  //ユーザーが自分のメールアドレスを所有していることを確認するためのリンクをメールで送信する
-    .then(() => {
-      alert('メールアドレス確認メールを送信しました。');
-    } )
-    .catch(err=>{
-      console.log(err),
-      alert('アカウントの作成に失敗しました。')
-      return Promise.reject(err)
-    })
-  }
+    async signup(account: Password): Promise<void> {
+        const userCredential = await this.afAuth.createUserWithEmailAndPassword(
+            account.email,
+            account.password,
+        );
+        // // .catch(err=>{
+        //   console.log(err),
+        //   alert('アカウントの作成に失敗しました。')
+        // })
+        try {
+            if (userCredential.user) {
+                console.log(account.username + 'です');
+                await userCredential.user.updateProfile({
+                    displayName: account.username,
+                });
+                await userCredential.user.sendEmailVerification();
+                alert('メールアドレス確認メールを送信しました。');
+            } else {
+                alert('userCredential.userが空');
+            }
+        } catch (error) {
+            alert('エラー');
+        }
+    }
 }
